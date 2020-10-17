b1da8cb90bce3733bb3aab3d3b0d99a5a1c883d7
fcdennis <fcdennis@gmail.com>
2020-10-17 12:33:21 -0300

    find your hat

---
+++ codecademy/find-your-hat/main.js
@@ -0,0 +1,12 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+const hat = '^';
+const hole = 'O';
+const fieldCharacter = '░';
+const pathCharacter = '*';
+
+class Field {
+    constructor(arr) {
+        this.arr = [[],[],[]]
+    }
+}
